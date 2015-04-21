fd13adf1de8ce897b57927c424a684eac1d0805e
WhizGit <whiztrain2@gmail.com>
2015-04-21 03:55:51 -0700

    add line

--- sauce-project/src/test/java/com/yourcompany/TestVerifyTitle.java
+++ sauce-project/src/test/java/com/yourcompany/TestVerifyTitle.java
@@ -38,11 +38,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
-import org.apache.poi.xssf.usermodel;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import org.apache.poi.poi.*;
+
 
 
 //-------------------------------------------------------------
