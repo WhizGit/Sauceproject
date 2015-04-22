d3710ec4cc4a13b22fc79ebb3d03dc811660138c
WhizGit <whiztrain2@gmail.com>
2015-04-22 01:32:44 -0700

    add

---
+++ sauce-project/src/test/java/com/yourcompany/TestVerifyTitle_1.java
@@ -0,0 +1,224 @@
+package com.saucelabs;
+
+
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import com.saucelabs.junit.Parallelized;
+import com.saucelabs.junit.ConcurrentParameterized;
+import com.saucelabs.junit.SauceOnDemandTestWatcher;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import java.net.URL;
+import java.util.LinkedList;
+import static org.junit.Assert.assertEquals;
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+import org.junit.Rule;
+//------------------------
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+//import org.apache.poi.poi.3/.11.*;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+//-------------------------------------------------------------
+
+/**
+ * Demonstrates how to write a JUnit test that runs tests against Sauce Labs using multiple browsers in parallel.
+ * <p/>
+ * The test also includes the {@link SauceOnDemandTestWatcher} which will invoke the Sauce REST API to mark
+ * the test as passed or failed.
+ *
+ * @author Ross Rowe
+ */
+@RunWith(ConcurrentParameterized.class)
+public class TestVerifyTitle implements SauceOnDemandSessionIdProvider {
+
+  public String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  public String Tar, Mtype, Mrec, Aggtype, Email, Password;
+  
+
+    /**
+     * Constructs a {@link SauceOnDemandAuthentication} instance using the supplied user name/access key.  To use the authentication
+     * supplied by environment variables or from an external file, use the no-arg {@link SauceOnDemandAuthentication} constructor.
+     */
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("ervarunvardhan", "dc9703ef-ad60-4c15-9caa-1279e5171c26");
+
+    /**
+     * JUnit Rule which will mark the Sauce Job as passed/failed when the test succeeds or fails.
+     */
+    @Rule
+    public SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this, authentication);
+
+    /**
+     * Represents the browser to be used as part of the test run.
+     */
+    private String browser;
+    /**
+     * Represents the operating system to be used as part of the test run.
+     */
+    private String os;
+    /**
+     * Represents the version of the browser to be used as part of the test run.
+     */
+    private String version;
+    /**
+     * Instance variable which contains the Sauce Job Id.
+     */
+    private String sessionId;
+
+    /**
+     * The {@link WebDriver} instance which is used to perform browser interactions with.
+     */
+    private WebDriver driver;
+
+    /**
+     * Constructs a new instance of the test.  The constructor requires three string parameters, which represent the operating
+     * system, version and browser to be used when launching a Sauce VM.  The order of the parameters should be the same
+     * as that of the elements within the {@link #browsersStrings()} method.
+     * @param os
+     * @param version
+     * @param browser
+     */
+    public TestVerifyTitle(String os, String version, String browser) {
+        super();
+        this.os = os;
+        this.version = version;
+        this.browser = browser;
+    }
+
+    /**
+     * @return a LinkedList containing String arrays representing the browser combinations the test should be run against. The values
+     * in the String array are used as part of the invocation of the test constructor
+     */
+    @ConcurrentParameterized.Parameters
+    public static LinkedList browsersStrings() {
+        LinkedList browsers = new LinkedList();
+        browsers.add(new String[]{"Windows 7", "30", "firefox"});
+      //  browsers.add(new String[]{"OSX 10.8", "6", "safari"});
+        return browsers;
+    }
+
+
+    /**
+     * Constructs a new {@link RemoteWebDriver} instance which is configured to use the capabilities defined by the {@link #browser},
+     * {@link #version} and {@link #os} instance variables, and which is configured to run against ondemand.saucelabs.com, using
+     * the username and access key populated by the {@link #authentication} instance.
+     *
+     * @throws Exception if an error occurs during the creation of the {@link RemoteWebDriver} instance.
+     */
+    @Before
+    public void setUp() throws Exception {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, browser);
+        if (version != null) {
+            capabilities.setCapability(CapabilityType.VERSION, version);
+        }
+        capabilities.setCapability(CapabilityType.PLATFORM, os);
+        capabilities.setCapability("name", "Chartlytics Sample Test");
+        this.driver = new RemoteWebDriver(
+                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
+                capabilities);
+        this.sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
+
+    }
+
+    /**
+     * Runs a simple test verifying the title of the amazon.com homepage.
+     * @throws Exception
+     */
+    @Test
+   public void testVerifyTitle() throws Exception {
+  String[][] getit = GetValue("./src/test/java/com/yourcompany/dataexcel.xlsx","signup",2);
+	baseUrl = getit[0][0]; 
+	 Email= getit[0][2];  
+	 Password=getit[0][3];
+
+    driver.get("http://"+ baseUrl + "/signin");
+    driver.findElement(By.name("username")).clear();
+    driver.findElement(By.name("username")).sendKeys(Email);
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys(Password);
+
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+  }// end of test
+  private String[][] GetValue(String Pathfile, String sheetName, int startrow) throws IOException{
+	  File excel= new File(Pathfile);
+	  FileInputStream fis = new FileInputStream(excel);
+	  @SuppressWarnings("resource")
+	XSSFWorkbook wb = new XSSFWorkbook(fis);
+	  XSSFSheet ws = wb.getSheet(sheetName);
+	  int colNum = ws.getRow(startrow).getLastCellNum();
+	  String [][] arrays = new String [1][colNum];
+	  for(int i=0;i<colNum;i++){
+		  XSSFRow row= ws.getRow(startrow);
+		  XSSFCell cell = row.getCell(i);
+		  arrays[0][i] = cellToString(cell);
+		 // System.out.println(arrays[0][i]);
+	  }
+	  return arrays;
+  }
+
+  private String cellToString(XSSFCell cell) {
+			Object result;
+			int type = cell.getCellType();
+	
+			switch(type)
+			{
+			case 0:
+				result = cell.getNumericCellValue();
+				break;
+			case 1:
+				result = cell.getStringCellValue();
+				break;
+			default:
+				throw new RuntimeException("there are no support for this type of cell");
+			}
+			
+			return result.toString();
+
+	}
+    /**
+     * Closes the {@link WebDriver} session.
+     *
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+	
+    /*
+     * @return the value of the Sauce Job id.
+     */
+    @Override
+    public String getSessionId() {
+        return sessionId;
+    }
+}
