934021235725a152d1d1b8fc8a1d87572b137bc5
WhizGit <whiztrain2@gmail.com>
2015-04-24 05:35:52 -0700

    modify test suit 2

--- sauce-project/src/test/java/com/yourcompany/TestSuit_2.java
+++ sauce-project/src/test/java/com/yourcompany/TestSuit_2.java
@@ -1140,43 +1140,7 @@ public class TestSuit_2 implements SauceOnDemandSessionIdProvider {
   }
   
   
-  
-  public String[][] GetValue(String Pathfile, String sheetName, int startrow) throws IOException{
-	   File excel= new File(Pathfile);
-	   FileInputStream fis = new FileInputStream(excel);
-	   @SuppressWarnings("resource")
-	 XSSFWorkbook wb = new XSSFWorkbook(fis);
-	   XSSFSheet ws = wb.getSheet(sheetName);
-	   int colNum = ws.getRow(startrow).getLastCellNum();
-	   String [][] arrays = new String [1][colNum];
-	   for(int i=0;i<colNum;i++){
-	    XSSFRow row= ws.getRow(startrow);
-	    XSSFCell cell = row.getCell(i);
-	    arrays[0][i] = cellToString(cell);
-	   // System.out.println(arrays[0][i]);
-	   }
-	   return arrays;
-	  }
-
-	   private String cellToString(XSSFCell cell) {
-	   Object result;
-	   int type = cell.getCellType();
-	 
-	   switch(type)
-	   {
-	   case 0:
-	    result = cell.getNumericCellValue();
-	    break;
-	   case 1:
-	    result = cell.getStringCellValue();
-	    break;
-	   default:
-	    throw new RuntimeException("there are no support for this type of cell");
-	   }
-	   
-	   return result.toString();
-
-	 }
+ 
 	
  
   // Methode GetValue is used to read the data from the excel sheet
