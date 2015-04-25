93e1b8d0a2aa5f51d1997a85147da59cb28ead73
WhizGit <whiztrain2@gmail.com>
2015-04-25 02:33:09 -0700

    modify

--- sauce-project/src/test/java/com/yourcompany/TestSuit_4.java
+++ sauce-project/src/test/java/com/yourcompany/TestSuit_4.java
@@ -304,22 +304,6 @@ public class TestSuit_4 implements SauceOnDemandSessionIdProvider {
 }
  
 
- 
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
    
 
     @After
